2328a958e779d641ce964394ce9a4ec1f801f352
Nikita Akopjans <nikita.akopjans@gmail.com>
2020-03-12 00:00:30 +0200

    Add sql scripts to generate db if EF Core fails
    - Add sql script to populate db with test data (accounts and financial transactions)

---
+++ Uzdevums2/Uzdevums2.Web/Data/Migrations/20200311140913_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uzdevums2.Web.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FinancialTransaction",
+                columns: table => new
+                {
+                    FinancialTransactionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FromUsername = table.Column<string>(nullable: false),
+                    ToUsername = table.Column<string>(maxLength: 256, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    IsLoan = table.Column<bool>(nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinancialTransaction", x => x.FinancialTransactionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinancialTransaction");
+        }
+    }
+}
